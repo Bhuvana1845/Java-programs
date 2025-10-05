3173bb4eba6e99cf6062293ab521fad4b4cffbd1
Bhuvana A M <bhuvanaam76@gmail.com>
2025-10-05 08:19:10 +0530

    pattern2.java
    Java program to print the pattern

---
+++ pattern2.java
@@ -0,0 +1,23 @@
+public class patterns{
+    public static void main(String[] args){
+        System.out.println("patterns:");
+            int n=4;//rows
+            int m=5;//cols
+                    /*
+         * * * * *
+         *       *
+         *       *
+         * * * * *
+         */
+           for(int i=1;i<=n;i++){
+               for(int j=1;j<=m;j++){
+                   if(i==1 || j==1 || i==n || j==m){
+                        System.out.print("* ");
+                   }else{
+                        System.out.print("  ");
+                    }
+               }
+              System.out.println();
+           }
+        System.out.println();
+}
