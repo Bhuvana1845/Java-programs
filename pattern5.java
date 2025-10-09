edad81f55b4586c6bcbb4897739168cd435f4ec4
Bhuvana A M <bhuvanaam76@gmail.com>
2025-10-09 22:31:59 +0530

    pattern5.java
    Java program to print the pattern
           * * * *
             * * *
             * *
             *

---
+++ pattern5.java
@@ -0,0 +1,18 @@
+public class patterns{
+    public static void main(String[] args){
+        System.out.println("patterns:");
+         /*
+         * * * *
+         * * *
+         * *
+         * 
+         */
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
